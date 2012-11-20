0cb6eb2d78edd4c2ef4e4c3a760e80dfcac86ba2
Хухлаев Александр <sancheolz@sancheolz-MS-7519.(none)>
2012-11-20 20:25:22 +0400

    rewrite to use in rhythmbox 2.96, ported to PyGI GTK3 and vk.com API 3.0

--- VkontakteSearch.py
+++ VkontakteSearch.py
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import rb
+import urllib
 import urllib2
 import hashlib
 import codecs
@@ -26,41 +27,21 @@ from html_decode import decode_htmlentities
 
 from VkontakteResult import VkontakteResult
 
-APP_ID = 1850196
-SECRET_KEY = 'nk0n6I6vjQ'
-USER_ID = 76347967
-
 def utf8ise(s):
 	return codecs.utf_8_encode(s)[0]
 
 class VkontakteSearch:
-	def __init__(self, search_term, db, entry_type):
+	def __init__(self, token, user_id, search_term, db, entry_type):
 		print "search_term ='%s'" % search_term
+		self.token = token
+		self.user_id = user_id
 		self.search_term = search_term
 		self.db = db
 		self.entry_type = entry_type
 		self.search_complete = False
 		self.entries_hashes = []
 		self.query_model = RB.RhythmDBQueryModel.new_empty(db)
-
-	def make_sig(self, params):
-		#str = "%sapi_id=%scount=300method=%sq=%stest_mode=1v=2.0%s" % (USER_ID, APP_ID, method, query, SECRET_KEY)
-		params_keys_sorted = params.keys()
-		params_keys_sorted.sort()
-		sig_obj = str(USER_ID)
-		for param in params_keys_sorted:
-			sig_obj += "%s=%s" % (param, params[param])
-		sig_obj += SECRET_KEY
-		return hashlib.md5(sig_obj).hexdigest()
 		
-	def make_url(self, params):
-		params_keys_sorted = params.keys()
-		params_keys_sorted.sort()
-		url_obj = "http://api.vk.com/api.php?"
-		for param in params_keys_sorted:
-			url_obj += "%s=%s&" % (param, urllib2.quote(str(params[param])))
-		return url_obj
-
 	def is_complete(self):
 		print "complete = %s" % self.search_complete
 		return self.search_complete
@@ -86,36 +67,47 @@ class VkontakteSearch:
 	def on_search_results_recieved(self, data):
 		# vkontakte sometimes returns invalid XML with empty first line
 		print "got data\n" + str(data)
-		data = data.lstrip()
+		if isinstance(data, str):
+			data = data.lstrip()
 		# remove invalid symbol that occured in titles/artist
 		#data = data.replace(u'\uffff', '')
-		xmldoc = minidom.parseString(data)
+			xmldoc = minidom.parseString(data)
 		# Check for errors
-		if xmldoc.getElementsByTagName("error_msg").length > 0:
-			self.error_msg = xmldoc.getElementsByTagName('error_msg')[0].firstChild.nodeValue.strip()
-			print "got error '%s'" % self.error_msg
+			if xmldoc.getElementsByTagName("error_msg").length > 0:
+				self.error_msg = xmldoc.getElementsByTagName('error_msg')[0].firstChild.nodeValue.strip()
+				print "got error '%s'" % self.error_msg
 
-		audios = xmldoc.getElementsByTagName("audio")
-		for audio in audios:
-			self.add_entry(VkontakteResult(audio))
-		self.search_complete = True
+			audios = xmldoc.getElementsByTagName("audio")
+			for audio in audios:
+				self.add_entry(VkontakteResult(audio))
+			self.search_complete = True
 
 	# Starts searching
 	def start(self):
 		print "start"
 		self.error_msg = ''
 		params = {}
-		params['api_id'] = APP_ID
 		params['count'] = '100'
-		params['v'] = '2.0'
-		params['method'] = 'audio.search'
+		params['v'] = '2.0'		
 		params['test_mode'] = '1'
 		params['auto_complete'] = '1'
 		params['sort'] = '2'
+		params['uid'] = self.user_id
 		params['q'] = self.search_term
-		params['sig'] = self.make_sig(params)
-		url = self.make_url(params)
-		#path = "http://api.vk.com/api.php?api_id=%s&count=300&v=2.0&method=audio.search&sig=%s&test_mode=1&q=%s" % (, )
+		#("q", self.search_term)
+		url = self.make_url('audio.search', ("q", self.search_term), self.token)		
 		print "path='%s'" % url
+		loader = rb.Loader()
+		loader.get_url(url, self.on_search_results_recieved)
+		
+	def make_url(self, method, params, token):
+		if isinstance(params, list):
+			params_list = [kv for kv in params]
+		elif isinstance(params, dict):
+			params_list = params.items()
+		else:
+			params_list = [params]
+		params_list.append(("access_token", token))
+		url = "https://api.vk.com/method/%s.xml?%s" % (method, urllib.urlencode(params_list)) 
 		loader = rb.Loader()
 		loader.get_url(url, self.on_search_results_recieved)
